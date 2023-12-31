cb1a90e3129dab456dc0de674297c7c95fc452a2
prince kumar <agarwal8789@gmail.com>
2023-12-31 14:07:04 +0530

    Props Commit made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { Learnjsx } from "./assets/Component/learnjsx"
 import { Learncomp } from "./assets/Component/Learncomp"
+import { Learnprops } from "./assets/Component/Learnprops";
 function App() {
-
+  let price=10;
   return (
     <>
       <h1>Hello Prince</h1>
       <Learnjsx/>{/*This is a component*/}
       <Learncomp/>{/*This is a component with variable*/}
+      <Learnprops name="Prince" price={price}/>
     </>
   )
 }
